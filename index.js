f1d34710857543e110eb8a7d8d110858a0359a43
RuanDevz <Ruanbatista1509@outlook.com>
2024-04-23 13:01:05 -0300

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const db = require('./models')
+const cors = require('cors')
 
+
+app.use(cors())
 const RouterTelefone = require('./routes/tels')
 app.use(express.json())
 
