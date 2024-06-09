9690a26e099a0f00cb77a42eefed446837cf7345
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-09 02:15:51 -0300

    supabase

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js'); // Importe o cliente do Supabase
 
 app.use(cors());
+
+const supabaseUrl = 'https://ympgtxpizjgrsrbtexry.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey); // Crie uma instância do cliente Supabase
 
 const RouterTelefone = require('./routes/tels');
 app.use(express.json());
 app.use('/telefone', RouterTelefone);
 
-// Configuração do Sequelize para conectar ao banco de dados
 db.sequelize.authenticate()
     .then(() => {
         console.log('Conexão com o banco de dados estabelecida com sucesso.');
