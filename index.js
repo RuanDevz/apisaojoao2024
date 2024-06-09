3bcd9f7a9afa0099b99cfe0267fe7602fd2fbb21
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-09 01:23:05 -0300

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const db = require('./models')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const db = require('./models');
+const cors = require('cors');
 
+app.use(cors());
 
-app.use(cors())
-const RouterTelefone = require('./routes/tels')
-app.use(express.json())
+const RouterTelefone = require('./routes/tels');
+app.use(express.json());
+app.use('/telefone', RouterTelefone);
 
-app.use('/telefone', RouterTelefone )
-
-
-db.sequelize.sync().then(() =>{
-    app.listen(process.env.PORT || 8080, () =>{
-        console.log("servidor rodando...")
+db.sequelize.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        return db.sequelize.sync()
     })
-})
+    .then(() => {
+        app.listen(process.env.PORT || 8080, () => {
+            console.log("Servidor rodando na porta 8080...");
+        });
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
