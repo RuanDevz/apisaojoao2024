8bf9516bba83473cd6537057f66372fe6c94211b
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-09 01:51:50 -0300

    dv

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const cors = require('cors');
-const dotenv = require('dotenv')
-
-dotenv.config()
 
 app.use(cors());
-
-
 
 const RouterTelefone = require('./routes/tels');
 app.use(express.json());
 app.use('/telefone', RouterTelefone);
 
+// Configuração do Sequelize para conectar ao banco de dados
 db.sequelize.authenticate()
     .then(() => {
         console.log('Conexão com o banco de dados estabelecida com sucesso.');
-        return db.sequelize.sync()
+        return db.sequelize.sync(); // Isso sincroniza os modelos com o banco de dados
     })
     .then(() => {
-        app.listen(process.env.PORT || 8080, () => {
-            console.log("Servidor rodando na porta 8080...");
+        const PORT = process.env.PORT || 8080;
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}...`);
         });
     })
     .catch(err => {
